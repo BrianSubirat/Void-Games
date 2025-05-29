fb46ddb2e32241a03a4fb59352f657e24972850a
7182 <Briansubirat6@gmail.com>
2025-05-29 17:09:46 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -90,41 +90,46 @@ function openGame(url, event) {
         particlesJS('particles-js', {
             particles: {
                 number: {
-                    value: 80,
+                    value: 100,
                     density: {
                         enable: true,
                         value_area: 800
                     }
                 },
                 color: {
-                    value: '#ffffff'
+                    value: ['#ff0000', '#00ff00', '#0000ff']
                 },
                 shape: {
                     type: 'circle'
                 },
                 opacity: {
-                    value: 0.5,
-                    random: false
+                    value: 0.3,
+                    random: true
                 },
                 size: {
-                    value: 3,
+                    value: 4,
                     random: true
                 },
                 line_linked: {
                     enable: true,
                     distance: 150,
                     color: '#ffffff',
-                    opacity: 0.4,
+                    opacity: 0.2,
                     width: 1
                 },
                 move: {
                     enable: true,
                     speed: 2,
                     direction: 'none',
-                    random: false,
+                    random: true,
                     straight: false,
                     out_mode: 'out',
-                    bounce: false
+                    bounce: false,
+                    attract: {
+                        enable: true,
+                        rotateX: 600,
+                        rotateY: 1200
+                    }
                 }
             },
             interactivity: {
@@ -132,9 +137,24 @@ function openGame(url, event) {
                 events: {
                     onhover: {
                         enable: true,
-                        mode: 'repulse'
+                        mode: 'grab'
+                    },
+                    onclick: {
+                        enable: true,
+                        mode: 'push'
                     },
                     resize: true
+                },
+                modes: {
+                    grab: {
+                        distance: 140,
+                        line_linked: {
+                            opacity: 1
+                        }
+                    },
+                    push: {
+                        particles_nb: 4
+                    }
                 }
             },
             retina_detect: true
